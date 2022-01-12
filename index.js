4983b9e0d2faa8795dd6fbe6043d6e97492cd84d
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-01-12 18:53:46 -0500

    solved task 3 and 4

--- index.js
+++ index.js
@@ -82,8 +82,10 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(array, flavor){
+  String(flavor);
+  array.unshift(flavor);
+  return array;
 }
 
 
@@ -98,8 +100,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(array){
+  array.pop();
+  return array;
 }
 
 
